de875f2175e87d9307c6647fb3ba7f21c92ef548
SUDHAN R <119998947+Srisudhan2003@users.noreply.github.com>
2024-03-26 11:15:22 +0530

    Add files via upload

---
+++ Backend/Day 6/giftapp/src/main/java/com/java/gift/model/Customer.java
@@ -0,0 +1,28 @@
+package com.java.gift.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long customerId;
+
+    private String customerName;
+    private String address;
+
+    @OneToOne
+    private User user;
+}
+
+
